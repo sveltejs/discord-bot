2f98e483b412100c14101180c65d70044cbdde8a
pngwn <hello@pngwn.io>
2021-12-10 13:49:04 +0000

    test

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ import polka from 'polka'
 
 polka()
     .get('/:name', (req, res) => {
-        res.end(`Hello, ${req.params.name}!`);
+        res.end(`Hello there, ${req.params.name}!`);
     })
     .listen(3000, () => {
         console.log(`> Running on localhost:3000`);
