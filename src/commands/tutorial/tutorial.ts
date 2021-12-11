46e0599f72a69ea229e24c5eb4c5bc9d0ac754e7
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-12-11 17:54:45 +0545

    Finish up tutorial command

--- src/commands/tutorial/tutorial.ts
+++ src/commands/tutorial/tutorial.ts
@@ -1,5 +1,97 @@
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import { command } from 'jellycommands';
+import fuzzysort from 'fuzzysort';
+
+// Might be a better idea to read this from a JSON file created in a build script
+const tutorials: Record<string, string> = {
+	Basics: 'basics',
+	'Adding data': 'adding-data',
+	'Dynamic attributes': 'dynamic-attributes',
+	Styling: 'styling',
+	'Nested components': 'nested-components',
+	'HTML tags': 'html-tags',
+	'Making an app': 'making-an-app',
+	Assignments: 'reactive-assignments',
+	Declarations: 'reactive-declarations',
+	Statements: 'reactive-statements',
+	'Updating arrays and objects': 'updating-arrays-and-objects',
+	'Declaring props': 'declaring-props',
+	'Default values': 'default-values',
+	'Spread props': 'spread-props',
+	'If blocks': 'if-blocks',
+	'Else blocks': 'else-blocks',
+	'Else-if blocks': 'else-if-blocks',
+	'Each blocks': 'each-blocks',
+	'Keyed each blocks': 'keyed-each-blocks',
+	'Await blocks': 'await-blocks',
+	'DOM events': 'dom-events',
+	'Inline handlers': 'inline-handlers',
+	'Event modifiers': 'event-modifiers',
+	'Component events': 'component-events',
+	'Event forwarding': 'event-forwarding',
+	'DOM event forwarding': 'dom-event-forwarding',
+	'Text inputs': 'text-inputs',
+	'Numeric inputs': 'numeric-inputs',
+	'Checkbox inputs': 'checkbox-inputs',
+	'Group inputs': 'group-inputs',
+	'Textarea inputs': 'textarea-inputs',
+	'Select bindings': 'select-bindings',
+	'Select multiple': 'multiple-select-bindings',
+	'Contenteditable bindings': 'contenteditable-bindings',
+	'Each block bindings': 'each-block-bindings',
+	'Media elements': 'media-elements',
+	Dimensions: 'dimensions',
+	This: 'bind-this',
+	'Component bindings': 'component-bindings',
+	'Binding to component instances': 'component-this',
+	onMount: 'onmount',
+	onDestroy: 'ondestroy',
+	'beforeUpdate and afterUpdate': 'update',
+	tick: 'tick',
+	'Writable stores': 'writable-stores',
+	'Auto-subscriptions': 'auto-subscriptions',
+	'Readable stores': 'readable-stores',
+	'Derived stores': 'derived-stores',
+	'Custom stores': 'custom-stores',
+	'Store bindings': 'store-bindings',
+	Tweened: 'tweened',
+	Spring: 'spring',
+	'The transition directive': 'transition',
+	'Adding parameters': 'adding-parameters-to-actions',
+	'In and out': 'in-and-out',
+	'Custom CSS transitions': 'custom-css-transitions',
+	'Custom JS transitions': 'custom-js-transitions',
+	'Transition events': 'transition-events',
+	'Local transitions': 'local-transitions',
+	'Deferred transitions': 'deferred-transitions',
+	'Key blocks': 'key-blocks',
+	'The animate directive': 'animate',
+	'The use directive': 'actions',
+	'The class directive': 'classes',
+	'Shorthand class directive': 'class-shorthand',
+	Slots: 'slots',
+	'Slot fallbacks': 'slot-fallbacks',
+	'Named slots': 'named-slots',
+	'Checking for slot content': 'optional-slots',
+	'Slot props': 'slot-props',
+	'setContext and getContext': 'context-api',
+	'<svelte:self>': 'svelte-self',
+	'<svelte:component>': 'svelte-component',
+	'<svelte:window>': 'svelte-window',
+	'<svelte:window> bindings': 'svelte-window-bindings',
+	'<svelte:body>': 'svelte-body',
+	'<svelte:head>': 'svelte-head',
+	'<svelte:options>': 'svelte-options',
+	'<svelte:fragment>': 'svelte-fragment',
+	'Sharing code': 'sharing-code',
+	Exports: 'module-exports',
+	'The @debug tag': 'debug',
+	'Congratulations!': 'congratulations',
+};
+
+const preppedTutorialTitles = Object.keys(tutorials).map((title) =>
+	fuzzysort.prepare(title),
+);
 
 export default command({
 	name: 'tutorial',
@@ -26,7 +118,30 @@ export default command({
 					},
 				],
 			});
-			return;
+		} else {
+			let results = fuzzysort.go(topic, preppedTutorialTitles);
+
+			if (results.total === 0) {
+				interaction.reply({
+					content: `No matching result found. Try again with a different search term.`,
+					ephemeral: true,
+				});
+			} else {
+				let topResult = results[0];
+
+				interaction.reply({
+					embeds: [
+						{
+							description: `Have you gone through the tutorial page on [${
+								topResult.target
+							}](https://svelte.dev/tutorial/${
+								tutorials[topResult.target]
+							})?`,
+							color: 0xff3e00,
+						},
+					],
+				});
+			}
 		}
 	},
 });
