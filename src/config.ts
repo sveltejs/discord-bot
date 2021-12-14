d533b258277e78925328acde33bb3968155e46d4
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-12-14 19:18:43 +0545

    Use NODE_ENV instead of MODE

--- src/config.ts
+++ src/config.ts
@@ -1,6 +1,6 @@
 import 'dotenv/config';
 
-export const DEV_MODE = process.env.MODE === 'DEV';
+export const DEV_MODE = process.env.NODE_ENV !== 'production';
 
 export const TEST_GUILD_ID = '918887934822858802';
 
@@ -8,36 +8,34 @@ export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
 export const SVELTE_ORANGE = 0xff3e00;
 
-export const LINK_ONLY_CHANNELS =
-	process.env.MODE === 'DEV'
-		? [
-				// #test-link-validation
-				'918915215368810566',
+export const LINK_ONLY_CHANNELS = DEV_MODE
+	? [
+			// #test-link-validation
+			'918915215368810566',
 
-				// #both-both-is-good
-				'919196322303725568',
-		  ]
-		: [
-				// Showcase
-				'479653552869081089',
+			// #both-both-is-good
+			'919196322303725568',
+	  ]
+	: [
+			// Showcase
+			'479653552869081089',
 
-				// Resources
-				'837012201444999248',
-		  ];
+			// Resources
+			'837012201444999248',
+	  ];
 
-export const AUTO_THREAD_CHANNELS =
-	process.env.MODE === 'DEV'
-		? [
-				// #test-auto-thread
-				'918932662226386994',
+export const AUTO_THREAD_CHANNELS = DEV_MODE
+	? [
+			// #test-auto-thread
+			'918932662226386994',
 
-				// #both-both-is-good
-				'919196322303725568',
-		  ]
-		: [
-				// Showcase
-				'479653552869081089',
+			// #both-both-is-good
+			'919196322303725568',
+	  ]
+	: [
+			// Showcase
+			'479653552869081089',
 
-				// Resources
-				'837012201444999248',
-		  ];
+			// Resources
+			'837012201444999248',
+	  ];
