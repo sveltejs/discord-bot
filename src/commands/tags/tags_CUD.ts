db09e365a5f8d18b37404948b9c8217dd6fe3eb8
Nayan Gautam <50981692+698969@users.noreply.github.com>
2021-12-18 17:38:03 +0545

    Closer to finishing tags

--- src/commands/tags/tags_CUD.ts
+++ src/commands/tags/tags_CUD.ts
@@ -1,8 +1,10 @@
-import { Message } from 'discord.js';
+import type { SupabaseClient } from '@supabase/supabase-js';
+import type { Message } from 'discord.js';
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import { command } from 'jellycommands';
 import { DEV_MODE } from '../../config';
 import { tagsEmbedBuilder } from '../../utils/embedBuilder';
+import type { Tag } from './tags_read';
 
 const enum Actions {
 	CREATE = 'create',
@@ -13,7 +15,6 @@ const enum Actions {
 export default command({
 	name: 'tags',
 	description: 'Create, edit or delete tags',
-	// disabled: true,
 	global: true,
 	dev: DEV_MODE,
 	options: [
@@ -58,12 +59,31 @@ export default command({
 		},
 	],
 
-	run: async ({ interaction }) => {
+	run: async ({ interaction, client }) => {
 		const subcommand = interaction.options.getSubcommand() as Actions;
-		const tagName = interaction.options.getString('name', true);
+		const tagName = interaction.options
+			.getString('name', true)
+			.toLowerCase();
+		const supabase: SupabaseClient = client.props.get('supabase');
+
+		const { data: tags, error } = await supabase
+			.from<Tag>('tags')
+			.select('*')
+			.eq('tag_name', tagName)
+			.limit(1);
+		if (error) return;
+		const tag = tags?.[0];
 
 		switch (subcommand) {
 			case Actions.CREATE: {
+				if (tag) {
+					await interaction.reply({
+						content:
+							'A tag with that name exists already. Did you mean to do `/tags update` instead?',
+						ephemeral: true,
+					});
+				}
+
 				await interaction.reply({
 					content:
 						'Send the contents for the tag in this channel within the next 60 seconds.',
@@ -84,16 +104,26 @@ export default command({
 					});
 					return;
 				}
-				await message?.delete();
+				await message.delete();
 				// All messages from the bot are ephemeral so feels kinda weird to have the message stick around
 
-				// TODO: Add the tag to the database, send an error if a tag with the same name exists already
+				const { error } = await supabase.from<Tag>('tags').insert({
+					tag_name: tagName,
+					tag_content: message.content,
+					author_id: interaction.user.id,
+				});
+				if (error) {
+					await interaction.editReply({
+						content: `There was an error in creating the tag "${tagName}". Tag names are case insensitive and should be unique.`,
+					});
+					return;
+				}
 				await interaction.editReply({
 					content: `Added tag "${tagName}".`,
 					embeds: [
 						tagsEmbedBuilder({
 							tagName,
-							tagContent: message?.content as string,
+							tagContent: message.content,
 							author: interaction.user,
 						}),
 					],
@@ -103,46 +133,78 @@ export default command({
 			}
 
 			case Actions.DELETE: {
-				const tag = {}; // Get the tag from the database
-				if (
-					true
-					/* TODO: Check if a tag with that name exists, 
-					and the command executor has the permissions to delete it 
-					(ie is the author or a moderator) 
-					*/
-				) {
+				if (!tag) {
 					await interaction.reply({
-						content:
-							"Either that tag doesn't exist or you don't have the permissions to delete it.",
+						content: 'No tag with that name exists.',
 						ephemeral: true,
 					});
 					return;
 				}
-				// Otherwise delete it from the db
+				if (
+					interaction.user.id !==
+					tag.author_id /*TODO: add moderator check*/
+				) {
+					await interaction.reply({
+						content:
+							"You don't have the permissions to delete that tag. You either have to be the author or a moderator.",
+						ephemeral: true,
+					});
+					return;
+				}
+
+				const { error } = await supabase
+					.from<Tag>('tags')
+					.delete()
+					.eq('id', tag.id);
+				if (error) {
+					await interaction.reply({
+						content: `Failed to delete tag "${tagName}".`,
+						ephemeral: true,
+					});
+					return;
+				}
+				await interaction.reply({
+					content: `Tag "${tagName}" was successfully deleted.`,
+					embeds: [
+						tagsEmbedBuilder({
+							tagName,
+							tagContent: tag.tag_content,
+							author: client.users.cache.get(tag.author_id),
+						}),
+					],
+					ephemeral: true,
+				});
 				break;
 			}
 
 			case Actions.UPDATE: {
-				const tag = {}; // Get the tag from the database
-				if (
-					true
-					/* TODO: Check if a tag with that name exists, 
-					and the command executor has the permissions to edit it 
-					(ie is the author) 
-					*/
-				) {
+				if (!tag) {
 					await interaction.reply({
 						content:
-							"Either that tag doesn't exist or you don't have the permissions to delete it. Only the author of the tag can delete it",
+							'No tag with that name exists. Did you mean to do `/tags create` instead?',
+						ephemeral: true,
+					});
+					return;
+				}
+				if (interaction.user.id !== tag.author_id) {
+					await interaction.reply({
+						content:
+							"You don't have the permissions to delete that tag. You have to be the author of the tag.",
 						ephemeral: true,
 					});
 					return;
 				}
 
 				await interaction.reply({
-					content:
-						'Send the new contents for the tag in this channel within the next 60 seconds.',
+					content: `Editing tag "${tagName}". Send the new contents for the tag in this channel within the next 60 seconds.`,
 					ephemeral: true,
+					embeds: [
+						tagsEmbedBuilder({
+							tagName,
+							tagContent: tag.tag_content,
+							author: client.users.cache.get(tag.author_id),
+						}),
+					],
 				});
 
 				let messageColl = await interaction.channel?.awaitMessages({
@@ -152,13 +214,37 @@ export default command({
 					max: 1,
 				});
 
-				if (!messageColl?.size) {
+				const message = messageColl?.first();
+				if (!message) {
 					await interaction.editReply({
 						content: 'No content received for the tag. Aborting.',
 					});
 					return;
 				}
-				// Otherwise update the tag in the database
+				await message.delete();
+
+				const { error } = await supabase
+					.from<Tag>('tags')
+					.update({ tag_content: message.content })
+					.eq('id', tag.id);
+
+				if (error) {
+					await interaction.editReply({
+						content: `Failed to update tag "${tagName}."`,
+					});
+					return;
+				}
+				await interaction.editReply({
+					content: `Tag "${tagName}" was successfully updated.`,
+					embeds: [
+						tagsEmbedBuilder({
+							tagName,
+							tagContent: message.content,
+							author: client.users.cache.get(tag.author_id),
+						}),
+					],
+				});
+				break;
 			}
 		}
 	},
--- src/commands/tags/tags_read.ts
+++ src/commands/tags/tags_read.ts
@@ -1,6 +1,8 @@
+import type { SupabaseClient } from '@supabase/supabase-js';
 import { ApplicationCommandOptionTypes } from 'discord.js/typings/enums';
 import { command } from 'jellycommands';
 import { DEV_MODE } from '../../config';
+import { tagsEmbedBuilder } from '../../utils/embedBuilder';
 
 export default command({
 	name: 'tag',
@@ -15,9 +17,44 @@ export default command({
 			required: true,
 		},
 	],
-	disabled: true,
 
-	run: async ({ interaction }) => {
-		const tagName = interaction.options.getString('name', true);
+	run: async ({ interaction, client }) => {
+		const tagName = interaction.options
+			.getString('name', true)
+			.toLowerCase(); // Make tag names case insensitive to disallow similar names and avoid confusion
+		const supabase: SupabaseClient = client.props.get('supabase');
+
+		const { data: tags } = await supabase
+			.from<Tag>('tags')
+			.select('*')
+			.eq('tag_name', tagName)
+			.limit(1);
+
+		const tag = tags?.[0];
+
+		if (!tag) {
+			await interaction.reply({
+				content:
+					'No tag found with that name, remember tag names have to be exact.',
+				ephemeral: true,
+			});
+			return;
+		}
+		await interaction.reply({
+			embeds: [
+				tagsEmbedBuilder({
+					tagName: tag.tag_name,
+					tagContent: tag.tag_content,
+					author: client.users.cache.get(tag.author_id),
+				}),
+			],
+		});
 	},
 });
+
+export interface Tag {
+	id: number;
+	tag_name: string;
+	tag_content: string;
+	author_id: string;
+}
