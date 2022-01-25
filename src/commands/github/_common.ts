7586fecc332fca6d364b0f8712f8e937d2a286fc
Nayan Gautam <50981692+698969@users.noreply.github.com>
2022-01-25 13:12:37 +0545

    refactor: github commands

--- src/commands/github/_common.ts
+++ src/commands/github/_common.ts
@@ -2,69 +2,113 @@ import { CommandInteraction } from 'discord.js';
 import fetch, { Headers } from 'node-fetch';
 import { GITHUB_TOKEN } from '../../config.js';
 import { listOfLinks } from '../../utils/embedBuilder.js';
-import { REPOS, REPO_DETAILS } from '../../utils/repositories.js';
+import { Repos, RepositoryDetails } from '../../utils/repositories.js';
 
-async function githubSearch(body: {
-	query: string;
-	variables: Record<string, string>;
-}) {
+const query = `
+query searchResults($search_string: String!, $type: SearchType!) {
+	search(type: $type, query: $search_string, first: 5) {
+		nodes {
+			... on Discussion {
+				title
+				number
+				url
+			}
+			... on Issue {
+				title
+				number
+				url
+			}
+			... on PullRequest {
+				title
+				number
+				url
+			}
+		}
+	}
+}`;
+
+/**
+ * Query the Github GraphQL API
+ *
+ * @returns `null` if there was an error or if there were 0 results
+ * otherwise a `string[]` of formatted links to the results.
+ */
+async function githubSearch(
+	search_string: string,
+	/**
+	 * The API doesn't distinguish between issues and PRs here.
+	 */
+	type: 'DISCUSSION' | 'ISSUE',
+) {
 	const res = await fetch('https://api.github.com/graphql', {
 		method: 'POST',
-		body: JSON.stringify(body),
+		body: JSON.stringify({
+			query,
+			variables: {
+				search_string,
+				type,
+			},
+		}),
 		headers: new Headers({
 			Authorization: `Bearer ${GITHUB_TOKEN}`,
 			'Content-Type': 'application/json',
 		}),
 	});
-	if (!res.ok) {
-		return null;
-	}
-	const results: Array<Record<string, any>> = ((await res.json()) as any).data
+	if (!res.ok) return null;
+
+	const results: Record<string, any>[] = ((await res.json()) as any).data
 		.search.nodes;
-	if (!results.length) {
-		return null;
-	}
-	return listOfLinks(
-		results.map(
-			(result) => `#[${result.number}](${result.url}): ${result.title}`,
-		),
-	);
+
+	return results?.length
+		? results.map(
+				(result) =>
+					`#[${result.number}](${result.url}): ${result.title}`,
+		  )
+		: null;
 }
 
 export async function githubCommandHandler(
 	interaction: CommandInteraction,
-	query: string,
-	is?: 'issue' | 'pr',
+	type: GithubResultType,
 ) {
-	const thisRepoDetails =
-		REPO_DETAILS[
-			interaction.options.getInteger('repository', true) as REPOS
-		];
+	const repoName =
+		RepositoryDetails[
+			interaction.options.getInteger('repository', true) as Repos
+		].NAME;
+
 	const topic = interaction.options.getString('topic');
 
-	const searchQuery = `${is ? `is:${is}` : ''} repo:${
-		thisRepoDetails.REPOSITORY_NAME
-	} ${topic || ''}`;
+	const search_string = `repo:${repoName} ${topic ?? ''} ${
+		type === GithubResultType.ISSUE
+			? 'is:issue'
+			: type === GithubResultType.PULL_REQUEST
+			? 'is:pr'
+			: '' // Discussion ignores the `is:` filter
+	}`;
 
 	try {
-		let results = await githubSearch({
-			query,
-			variables: {
-				searchQuery,
-			},
-		});
+		const results = await githubSearch(
+			search_string,
+			type === GithubResultType.DISCUSSION ? 'DISCUSSION' : 'ISSUE',
+		);
 
-		if (results) {
-			await interaction.reply({
-				embeds: [results],
-			});
-		} else {
-			await interaction.reply({
-				content: 'No results found.',
-				ephemeral: true,
-			});
-		}
+		await interaction.reply(
+			results
+				? {
+						embeds: [listOfLinks(results)],
+				  }
+				: {
+						content: 'No results found.',
+						ephemeral: true,
+				  },
+		);
 	} catch {
 		// TODO: Do nothing or log the error or something
 	}
+}
+
+export const enum GithubResultType {
+	ISSUE,
+	PULL_REQUEST,
+	DISCUSSION,
 }
