334952547e83d74556ecda65a9cedd89fa5c44c3
gtm-nayan <gtmnayan@gmail.com>
2022-01-31 12:03:06 +0545

    Read TEST_GUILD_ID from env file
    Makes it easier for other contributors since they might not have access to the default guild

--- src/config.ts
+++ src/config.ts
@@ -2,7 +2,7 @@ import 'dotenv/config';
 
 export const DEV_MODE = process.env.NODE_ENV !== 'production';
 
-export const TEST_GUILD_ID = '918887934822858802';
+export const TEST_GUILD_ID = process.env.TEST_GUILD_ID ?? '918887934822858802';
 
 export const DISCORD_TOKEN = process.env.DISCORD_TOKEN;
 
