c4ac657af37cef6b5b7b21f8697c447b330faf66
GHOSt <ghostdevbusiness@gmail.com>
2022-02-09 16:27:03 +0000

    refactor: remove buttons

--- src/events/on_message_auto_thread.ts
+++ src/events/on_message_auto_thread.ts
@@ -1,6 +1,5 @@
 import { Message, MessageOptions, ThreadChannel } from 'discord.js';
 import { AUTO_THREAD_CHANNELS, HELP_CHANNELS } from '../config.js';
-import { MessageActionRow, MessageButton } from 'discord.js';
 import { get_title_from_url } from '../utils/unfurl.js';
 import { add_thread_prefix } from '../utils/threads.js';
 import { build_embed } from '../utils/embed_helpers.js';
@@ -53,39 +52,14 @@ function instruction_message(thread: ThreadChannel): MessageOptions {
 	const base_description =
 		"I've created a thread for your message. Please continue any relevant discussion in this thread. You can rename it with the `/thread rename` command if I failed to set a proper name for it.";
 
-	if (!HELP_CHANNELS.includes(thread.parentId!))
-		return {
-			embeds: [
-				build_embed({
-					description: base_description,
-				}),
-			],
-		};
-
-	const archiveButton = new MessageButton({
-		label: 'Archive',
-		customId: 'thread-archive',
-		emoji: '🔒',
-		style: 'SECONDARY',
-	});
-
-	const solveButton = new MessageButton({
-		label: 'Solved',
-		customId: 'thread-solved',
-		emoji: '✅',
-		style: 'PRIMARY',
-	});
-
-	const row = new MessageActionRow({
-		components: [solveButton, archiveButton],
-	});
+	const description = HELP_CHANNELS.includes(thread.parentId!)
+		? `${base_description}\n\nWhen your problem is solved run \`/thread solve\`, don't forget to credit the person that helped you!`
+		: base_description;
 
 	return {
-		components: [row],
-
 		embeds: [
 			build_embed({
-				description: `${base_description}\n\nWhen your problem is solved you can archive it with \`/thread archive\`\n\nWhen your problem is solved run \`/thread solve\`, don't forget to credit the person that helped you!`,
+				description,
 			}),
 		],
 	};
