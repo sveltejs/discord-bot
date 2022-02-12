95692c025c3e9690ae5500a1436fef0018d7cb3b
gtm-nayan <gtmnayan@gmail.com>
2022-02-12 09:40:26 +0545

    ...

--- src/utils/tags.ts
+++ src/utils/tags.ts
@@ -15,19 +15,22 @@ export async function get_tags_list(
 	const prev_button = new MessageButton()
 		.setLabel('Previous')
 		.setCustomId(`tags_page_${page_number - 1}`)
-		.setDisabled(!(page_number > 1))
+		.setDisabled(page_number <= 1)
 		.setStyle('PRIMARY');
 
 	const next_button = new MessageButton()
 		.setLabel('Next')
 		.setCustomId(`tags_page_${page_number + 1}`)
-		.setDisabled(!(data.length === 10))
+		// @todo This is not robust, will give the wrong result when we have
+		// exactly a multiple of 10 tags
+		.setDisabled(data.length !== 10)
 		.setStyle('PRIMARY');
 
 	const row = new MessageActionRow({
 		components: [prev_button, next_button],
 	});
 
+	/** @todo Figure out a nice way to format these */
 	const tags = data.map(
 		(tag) => `\`${tag.tag_name.padEnd(32)}\` | <@${tag.author_id}>`,
 	);
