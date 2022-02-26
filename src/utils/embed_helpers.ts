5a39abbb239d4a4a9dc58222cb1671437e88937b
gtm-nayan <gtmnayan@gmail.com>
2022-02-26 11:17:20 +0545

    Create wrap in embed helper

--- src/utils/embed_helpers.ts
+++ src/utils/embed_helpers.ts
@@ -1,8 +1,22 @@
-import { GuildMember, MessageEmbed, MessageEmbedOptions } from 'discord.js';
+import {
+	GuildMember,
+	MessageEmbed,
+	MessageEmbedOptions,
+	MessageOptions,
+} from 'discord.js';
 import { SVELTE_ORANGE } from '../config.js';
 
 export const build_embed = (options: MessageEmbedOptions) =>
 	new MessageEmbed({ color: SVELTE_ORANGE, ...options });
+
+export function wrap_in_embed(
+	content: string,
+	embed_options?: MessageEmbedOptions,
+): MessageOptions {
+	const embed = build_embed(embed_options!);
+	embed.setDescription(content);
+	return { embeds: [embed] };
+}
 
 export function list_embed_builder(list_items: string[], title?: string) {
 	return build_embed({
