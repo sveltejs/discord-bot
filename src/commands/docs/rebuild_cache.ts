203f43d108684992a78f6029e9852fa6c83174ed
gtm-nayan <gtmnayan@gmail.com>
2022-04-15 21:56:18 +0545

    Fix rebuildcache

--- src/commands/docs/rebuild_cache.ts
+++ src/commands/docs/rebuild_cache.ts
@@ -1,6 +1,7 @@
 import { command } from 'jellycommands';
 import { BOT_DEVS } from '../../config';
 import { Repos } from '../../utils/repositories';
+import { get_member, has_any_role_or_id } from '../../utils/snowflake';
 import { build_cache as build_docs_cache } from './_docs_cache';
 import { build_cache as build_tutorials_cache } from './_tutorials_cache';
 
@@ -10,17 +11,17 @@ export default command({
 	defer: {
 		ephemeral: true,
 	},
-	guards: {
-		mode: 'whitelist',
-		users: BOT_DEVS,
-	},
+	global: true,
 	run: async ({ interaction }) => {
-		return await Promise.all([
-			build_docs_cache(Repos.SVELTE),
-			build_docs_cache(Repos.SVELTE_KIT),
-			build_tutorials_cache(),
-		])
-			.catch((e) => interaction.followUp(e))
-			.then(() => interaction.followUp('Success'));
+		const member = await get_member(interaction);
+
+		if (member && has_any_role_or_id(member, BOT_DEVS))
+			await Promise.all([
+				build_docs_cache(Repos.SVELTE),
+				build_docs_cache(Repos.SVELTE_KIT),
+				build_tutorials_cache(),
+			])
+				.catch((e) => interaction.followUp(e))
+				.then(() => interaction.followUp('Success'));
 	},
 });
