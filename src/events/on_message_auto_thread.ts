95767c545bbc9569ca2d7f4b496b58d6ae2b37c4
gtm-nayan <gtmnayan@gmail.com>
2022-04-18 20:09:01 +0545

    refactor: extract link predicate

--- src/events/on_message_auto_thread.ts
+++ src/events/on_message_auto_thread.ts
@@ -9,34 +9,31 @@ import {
 import { wrap_in_embed } from '../utils/embed_helpers.js';
 import { add_thread_prefix } from '../utils/threads.js';
 import { get_title_from_url } from '../utils/unfurl.js';
+import { fails_link_test } from './on_message_check_links.js';
 
 export default event({
 	name: 'messageCreate',
 	run: async ({}, message) => {
 		const should_ignore =
 			message.author.bot ||
-			message.hasThread ||
 			message.channel.type != 'GUILD_TEXT' ||
-			(LINK_ONLY_CHANNELS.includes(message.channel.id) && // It's going to be deleted in this case, which could cause an orphan thread
-				!url_regex().test(message.content));
+			!AUTO_THREAD_CHANNELS.includes(message.channelId) ||
+			fails_link_test(message); // If it fails link test then it'll get deleted and the thread will be orphaned
 
 		if (should_ignore) return;
 
-		if (AUTO_THREAD_CHANNELS.includes(message.channel.id)) {
-			const raw_name = await get_thread_name(message);
-			const prefixed = add_thread_prefix(raw_name, false);
+		const raw_name = await get_thread_name(message);
 
-			const name = HELP_CHANNELS.includes(message.channelId)
-				? prefixed
-				: raw_name;
+		const name = HELP_CHANNELS.includes(message.channelId)
+			? add_thread_prefix(raw_name, false)
+			: raw_name;
 
-			await message.channel.threads
-				.create({
-					name: name.slice(0, 100),
-					startMessage: message,
-				})
-				.then(send_instruction_message);
-		}
+		await message.channel.threads
+			.create({
+				name: name.slice(0, 100),
+				startMessage: message,
+			})
+			.then(send_instruction_message);
 	},
 });
 
--- src/events/on_message_check_links.ts
+++ src/events/on_message_check_links.ts
@@ -1,3 +1,4 @@
+import { Message } from 'discord.js';
 import { event } from 'jellycommands';
 import url_regex from 'url-regex';
 import { LINK_ONLY_CHANNELS } from '../config.js';
@@ -9,20 +10,23 @@ export default event({
 	run: async ({}, message) => {
 		if (message.author.bot) return;
 
-		if (LINK_ONLY_CHANNELS.includes(message.channel.id)) {
-			const has_link = url_regex().test(message.content);
+		if (fails_link_test(message)) {
+			if (message.deletable) await message.delete();
 
-			if (!has_link) {
-				if (message.deletable) await message.delete();
+			await message.author.send(
+				wrap_in_embed(
+					`Your message in ${message.channel.toString()} was removed since it doesn't contain a link, if you are trying to showcase a project please post a link with your text. Otherwise all conversation should be inside a thread\n\nYour message was sent below so you don't lose it!`,
+				),
+			);
 
-				await message.author.send(
-					wrap_in_embed(
-						`Your message in ${message.channel.toString()} was removed since it doesn't contain a link, if you are trying to showcase a project please post a link with your text. Otherwise all conversation should be inside a thread\n\nYour message was sent below so you don't lose it!`,
-					),
-				);
-
-				await message.author.send(message.content);
-			}
+			await message.author.send(message.content);
 		}
 	},
 });
+
+export function fails_link_test(message: Message) {
+	return (
+		LINK_ONLY_CHANNELS.includes(message.channelId) &&
+		!url_regex().test(message.content)
+	);
+}
