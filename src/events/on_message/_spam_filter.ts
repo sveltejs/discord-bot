1abdcff65ad5042ef58da256e82c6dd405c4805f
gtmnayan <gtmnayan@gmail.com>
2022-08-05 20:17:01 +0545

    link to ban appeal thread

--- src/events/on_message/_spam_filter.ts
+++ src/events/on_message/_spam_filter.ts
@@ -1,5 +1,6 @@
 import { GuildMember, Message } from 'discord.js';
 import { DEV_MODE, THREAD_ADMIN_IDS } from '../../config';
+import { no_op } from '../../utils/promise';
 import { RateLimitStore } from '../../utils/ratelimit';
 import { has_any_role_or_id } from '../../utils/snowflake';
 import { has_link, STOP } from './_common';
@@ -19,6 +20,11 @@ export default async function spam_filter(message: Message) {
 			message.member &&
 			!has_any_role_or_id(message.member, THREAD_ADMIN_IDS) // Possibly banter, can be manually reviewed.
 		) {
+			message.member
+				.send(
+					'You were banned from the Svelte discord server for spamming. If you believe this was a mistake you can appeal the ban at https://github.com/pngwn/svelte-bot/issues/38',
+				)
+				.catch(no_op);
 			await ban(message.member);
 		}
 
