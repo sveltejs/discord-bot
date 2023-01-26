22da2d2ad49ccaee58a0bfcafcda7002b6669138
gtmnayan <gtmnayan@gmail.com>
2023-01-26 14:00:12 +0545

    fix spam filter

--- src/events/on_message/_spam_filter.ts
+++ src/events/on_message/_spam_filter.ts
@@ -10,29 +10,26 @@ const limit = new RateLimitStore(3, 10_000);
 
 export default async function spam_filter(message: Message) {
 	const is_likely_spam =
-		has_link(message) &&
+		message.inGuild() &&
 		!message.thread &&
+		has_link(message) &&
 		limit.is_limited(message.author.id, true);
 
-	if (!is_likely_spam || !message.member) return;
+	if (!is_likely_spam) return;
+
+	const member = await message.guild.members.fetch(message.author.id);
+	const is_threadlord = has_any_role_or_id(member, THREAD_ADMIN_IDS);
 
 	if (DEV_MODE) {
 		await message.reply('Oi, stop spamming you troglodyte.');
-	} else {
-		const is_threadlord = has_any_role_or_id(
-			message.member,
-			THREAD_ADMIN_IDS,
-		);
-
 		// Unlikely to be spam from trusted members
-		if (!is_threadlord) {
-			await Promise.allSettled([
-				ban(message.member, 3),
-				message.member.send(
-					'You were banned from the Svelte discord server for spamming. If you believe this was a mistake you can appeal the ban at https://github.com/pngwn/svelte-bot/issues/38',
-				),
-			]);
-		}
+	} else if (!is_threadlord) {
+		await Promise.allSettled([
+			ban(member, 3),
+			member.send(
+				'You were banned from the Svelte discord server for spamming. If you believe this was a mistake you can appeal the ban at https://github.com/pngwn/svelte-bot/issues/38',
+			),
+		]);
 	}
 
 	throw STOP;
