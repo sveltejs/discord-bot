fddefe772abc7112bae56a3c0e45493d30b50f92
GHOST <ghostdevbusiness@gmail.com>
2024-10-31 05:29:13 +0000

    chore: remove supabase

--- src/db/supabase.ts
+++
@@ -1,7 +0,0 @@
-import { createClient } from '@supabase/supabase-js';
-import { Database } from './types';
-
-export const supabase = createClient<Database>(
-	process.env['SUPABASE_URL']!,
-	process.env['SUPABASE_KEY']!,
-);
