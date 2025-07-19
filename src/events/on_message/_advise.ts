f6ba448a71c7adf1dc65a918fb3534e144f416ee
GHOST <ghostdevbusiness@gmail.com>
2025-07-19 17:11:43 +0100

    chore: update

--- src/events/on_message/_advise.ts
+++ src/events/on_message/_advise.ts
@@ -1,67 +1,56 @@
-import { ChannelType, type Message } from 'discord.js';
-import { has_link } from './_common.js';
 import urlRegex from 'url-regex';
-
-const replacementMap = [
-	{ test: 'https://x.com', replace: 'https://xcancel.com' },
-];
+import {
+	type Message,
+	ChannelType,
+	hideLinkEmbed,
+	codeBlock,
+} from 'discord.js';
 
 export default async function mutate_content(message: Message) {
-	if (
-		message.channel.type != ChannelType.GuildText ||
-		(!has_link(message) && !message.content.includes('x.com'))
-	)
-		return;
+	if (message.channel.type != ChannelType.GuildText) return;
 
-	// Get links
-	const caughtLinks: string[] | undefined = message.content
+	const links = message.content
 		.match(urlRegex())
-		?.filter(
-			(link) =>
-				replacementMap.findIndex((item) =>
-					link.startsWith(item.test),
-				) !== -1,
+		?.filter((link) => link.startsWith('https://x.com'))
+		.map((link) =>
+			link.replace(/^https:\/\/x\.com/, 'https://xcancel.com'),
 		);
-	if (!caughtLinks) return;
 
-	const hasXLinks: boolean = caughtLinks.some((item) =>
-		item.startsWith('https://x.com'),
-	);
-	if (!hasXLinks) return;
+	if (!links || links.length === 0) {
+		return;
+	}
 
-	const updatedPhrase: string =
-		caughtLinks.length > 1
+	const updated_phrase: string =
+		links.length > 1
 			? 'Here are the updated links:'
 			: 'Here is the updated link:';
 
-	const updatedLinks = caughtLinks.map((link) => {
-		const replaceIdx = replacementMap.findIndex((item) =>
-			link.startsWith(item.test),
-		);
-		return link.replace(
-			replacementMap[replaceIdx].test,
-			replacementMap[replaceIdx].replace,
-		);
-	});
+	const updated_link_list = links
+		.map((link) => `- ${hideLinkEmbed(link)}`)
+		.join('\n');
 
-	const updatedLinkList = updatedLinks.map((link) => `- ${link}\n`).join('');
+	const updated_content = message.content.replace(urlRegex(), (match) => {
+		return match.startsWith('https://x.com')
+			? match.replace(/^https:\/\/x\.com/, 'https://xcancel.com')
+			: match;
+	});
 
 	try {
 		await message.author.send(
-			`Re: ${message.url}\n\nI see you've provided a link to \`x.com\`. Please consider posting a new message having \`x.com\` replaced with \`xcancel.com\`, that way server members may view the message and thread without requiring an account.\n\n${updatedPhrase}\`\`\`${updatedLinkList}\`\`\`\n\nHere is your entire message with adjusted links:\n\`\`\`${updatedLinkList}\`\`\``,
+			`Re: ${message.url}\n\nI see you've provided a link to \`x.com\`. Please consider posting a new message having \`x.com\` replaced with \`xcancel.com\`, that way server members may view the message and thread without requiring an account.\n\n${updated_phrase}\n${updated_link_list}\n\nHere is your entire message with adjusted links:\n${codeBlock(updated_content)}`,
 		);
 
 		return; // mission complete
-	} catch (_) {
+	} catch {
 		// assume user disabled DMs upon error
 	}
 
 	// Plan B: inline reply
 	try {
 		await message.reply(
-			`I converted your \`x.com\` links to use \`xcancel.com\` so that server members won't require an account to view content and threads.\n\n${updatedLinkList}`,
+			`I converted your \`x.com\` links to use \`xcancel.com\` so that server members won't require an account to view content and threads:\n${updated_link_list}`,
 		);
-	} catch (_) {
+	} catch {
 		// don't handle failures
 	}
 }
