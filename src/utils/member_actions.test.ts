628c63fb4f6b66bf5bd7f91d38202ed10e5c7a46
Enrico Sacchetti <enrico@theetrain.ca>
2026-07-26 23:55:09 -0400

    refactor(member actions): add options to timeout util
    - Add unit tests

---
+++ src/utils/member_actions.test.ts
@@ -0,0 +1,25 @@
+import { describe, it, mock } from 'node:test'
+import assert from 'node:assert/strict'
+import { timeout } from './member_actions.ts'
+
+describe('timeout', () => {
+  it('times out member', () => {
+    const member = {
+      timeout: mock.fn()
+    }
+
+    // @ts-ignore
+    timeout(member)
+    assert.deepStrictEqual(member.timeout.mock.calls[0].arguments, [43_200_000, 'Bot action'])
+  })
+
+  it('times out member with custom reason', () => {
+    const member = {
+      timeout: mock.fn()
+    }
+
+    // @ts-ignore
+    timeout(member, { reason: 'just because'})
+    assert.deepStrictEqual(member.timeout.mock.calls[0].arguments, [43_200_000, 'just because'])
+  })
+})
