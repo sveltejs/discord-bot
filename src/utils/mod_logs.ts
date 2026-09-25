103be62eed95d29186ba00c20b5703fcfa4f7cfc
Enrico Sacchetti <enrico@theetrain.ca>
2026-09-25 14:14:00 -0400

    feat: log message deletions (#88)
    * feat: log message deletions
    
    * feat: set up message copying
    
    - Use embeds to format copied and logged messages
    - Refactor other log messages to use embeds
    
    * chore: appease the linter
    
    * chore: remove unused method
    
    * chore: fmt
    
    ---------
    
    Co-authored-by: Willow (GHOST) <git@willow.sh>

--- src/utils/mod_logs.ts
+++ src/utils/mod_logs.ts
@@ -1,5 +1,12 @@
-import { ChannelType, type Message, type Client } from 'discord.js';
+import {
+	ChannelType,
+	type Message,
+	type Client,
+	type TextChannel,
+	EmbedBuilder,
+} from 'discord.js';
 import { MOD_LOGS_CHANNEL } from '../config.ts';
+import { LOG_LEVEL_COLOURS } from './embed_helpers.ts';
 
 /** Send a message to a moderator-visible channel */
 export async function mod_log(client: Client, message: string) {
@@ -12,7 +19,35 @@ export async function mod_log(client: Client, message: string) {
 	}
 }
 
-/** Forward a message to a moderator-visible channel */
-export async function mod_forward(message: Message) {
-	await message.forward(MOD_LOGS_CHANNEL);
+/** Copy a message to a moderator-visible channel */
+export async function mod_copy(
+	message: Message,
+	options?: {
+		title?: string;
+		pre_content?: string;
+		level?: (typeof LOG_LEVEL_COLOURS)[keyof typeof LOG_LEVEL_COLOURS];
+	},
+) {
+	const mod_logs_channel =
+		await message.client.channels.fetch(MOD_LOGS_CHANNEL);
+
+	if (mod_logs_channel?.isTextBased()) {
+		const pre = options?.pre_content ? `${options.pre_content}\n\n` : '';
+		const content = pre + message.content || '*No text content*';
+
+		const embed = new EmbedBuilder()
+			.setAuthor({
+				name: message.author.tag,
+				iconURL: message.author.displayAvatarURL(),
+			})
+			.setTitle(options?.title ?? null)
+			.setDescription(content)
+			.setColor(options?.level ?? LOG_LEVEL_COLOURS.INFO)
+			.setTimestamp(message.createdAt);
+
+		await (mod_logs_channel as TextChannel).send({
+			embeds: [embed],
+			files: message.attachments.map((a) => a.url),
+		});
+	}
 }
